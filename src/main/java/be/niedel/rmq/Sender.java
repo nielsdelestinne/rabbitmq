bef7bf1eee1cef9388aa696882793d1790bbef18
nielsdelestinne <nielsdelestinne@gmail.com>
2019-12-09 19:52:56 +0100

    simple send of message

---
+++ src/main/java/be/niedel/rmq/Sender.java
@@ -0,0 +1,33 @@
+package be.niedel.rmq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Sender {
+
+    private static final String QUEUE_NAME = "hello";
+    private static final String HOST = "localhost";
+
+    public static void main(String[] args) {
+
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost(HOST);
+        try(Connection connection = connectionFactory.newConnection();
+            final Channel channel = connection.createChannel()) {
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            String message = "Hello World!";
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+            System.out.println("[x] Sent '" + message + "'");
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
