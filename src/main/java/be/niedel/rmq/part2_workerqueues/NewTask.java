52ee2eb3856cf1ffe64dee994356a5437250975e
nielsdelestinne <nielsdelestinne@gmail.com>
2019-12-09 21:11:24 +0100

    Task Queue finished

--- src/main/java/be/niedel/rmq/part2_workerqueues/NewTask.java
+++ src/main/java/be/niedel/rmq/part2_workerqueues/NewTask.java
@@ -3,13 +3,14 @@ package be.niedel.rmq.part2_workerqueues;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
 public class NewTask {
 
-    private static final String QUEUE_NAME = "hello";
+    private static final String QUEUE_NAME = "task_queue";
     private static final String HOST = "localhost";
 
     public static void main(String[] args) {
@@ -18,10 +19,12 @@ public class NewTask {
         connectionFactory.setHost(HOST);
         try(Connection connection = connectionFactory.newConnection();
             final Channel channel = connection.createChannel()) {
-            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            channel.queueDeclare(QUEUE_NAME, true, false, false, null);
             String message = "Hello" + ".".repeat((int)(Math.random()*10));
-            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
-            System.out.println("[x] Sent '" + message + "'");
+            channel.basicPublish("", QUEUE_NAME,
+                    MessageProperties.PERSISTENT_TEXT_PLAIN,
+                    message.getBytes("UTF-8"));
+            System.out.println(" [x] Sent '" + message + "'");
         } catch (TimeoutException e) {
             e.printStackTrace();
         } catch (IOException e) {
--- src/main/java/be/niedel/rmq/part2_workerqueues/Worker.java
+++ src/main/java/be/niedel/rmq/part2_workerqueues/Worker.java
@@ -18,35 +18,46 @@ import java.util.concurrent.TimeoutException;
  */
 public class Worker {
 
-    private static final String QUEUE_NAME = "hello";
+    private static final String TASK_QUEUE_NAME = "task_queue";
 
-    public static void main(String[] args) throws IOException, TimeoutException {
+    public static void main(String[] argv) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
-        Connection connection = factory.newConnection();
-        Channel channel = connection.createChannel();
+        final Connection connection = factory.newConnection();
+        final Channel channel = connection.createChannel();
 
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
+        // This tells RabbitMQ not to give more than one message to a worker at a time.
+        // Or, in other words, don't dispatch a new message to a worker until it has processed and
+        // acknowledged the previous one. Instead, it will dispatch it to the next worker that is not still busy.
+        // This no longer follows the round-robin approach
+        channel.basicQos(1);
+
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+            String message = new String(delivery.getBody(), "UTF-8");
+
             System.out.println(" [x] Received '" + message + "'");
             try {
                 doWork(message);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
             } finally {
-                System.out.println("[ x] Done");
+                System.out.println(" [x] Done");
+                channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
             }
         };
-        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> {
-        });
+        channel.basicConsume(TASK_QUEUE_NAME, false, deliverCallback, consumerTag -> { });
     }
 
-    private static void doWork(String task) throws InterruptedException {
+    private static void doWork(String task) {
         for (char ch : task.toCharArray()) {
-            if (ch == '.') Thread.sleep(10000);
+            if (ch == '.') {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException _ignored) {
+                    Thread.currentThread().interrupt();
+                }
+            }
         }
     }
 
